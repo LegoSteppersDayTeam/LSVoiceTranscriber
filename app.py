7e6d2269206fad87fd30cd771aef587c0e51d4a4
JayDeezus <incrediblejoewindsor@gmail.com>
2024-08-19 09:30:24 -0700

    Think this is a dead end.  This approach was assuming the bot would listen in on voice chat, but it seems big-D Discord doesn't support that as a bot behavior, and discord.py doesn't seem to support that out of the box either.
    I'm switching approaches here, and will just have it transcribe from my mic
    locally and then send the text to a designated discord text channel.
    
    I'm still in the "make it work" phase.
    
    Signed-off-by: JayDeezus <incrediblejoewindsor@gmail.com>

--- app.py
+++ app.py
@@ -19,38 +19,170 @@ If the user of the /transcribe command is not authorized to use the command, an 
 
 import asyncio
 import discord
+from discord import app_commands
+from discord.ext import commands, voice_recv
+import time
 from RealtimeSTT import AudioToTextRecorder
 
 """
 Discord API variables
 Permissions: Send Messages, Connect
-permissions_integer=1050624
+permissions_integer=1050624  # probably not needed
 """
-client = discord.Client(intents=intents, activity=discord.CustomActivity(name=time.strftime("Started %d %b %y %H%M %z")))
 
-"""
+MY_GUILD = discord.Object(id=818478021563908116)
+transcription_ok_roles = {
+    'housekeeping': 818490394123698177,
+    'housekeeping_emeritus': 1062177151371198504,
+    'host': 819233684136788009,
+    'transcription_approved': 1274617618296340521
+}
+
+transcription_tasks = {}
+active_voiceclient = None
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents, activity: discord.CustomActivity):
+        super().__init__(intents=intents, activity=activity)
+        self.tree = app_commands.CommandTree(self)
+        
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+intents = discord.Intents.default()
+intents.voice_states = True
+client = MyClient(intents=intents, activity=discord.CustomActivity(name="Voice Transcriber Experiment Bot"))
+
+def text_detected(text):
+    #do something with text
+    pass
+
 # configure this to be fed audio data from discord.py.  I'm handing voice data to RSTT for transcription, it's not recording sound from my mic on its own.
 recorder_config = {
         'spinner': False,
         'model': 'large-v2',
         'silero_sensitivity': 0.4,
         'webrtc_sensitivity': 2,
-        'post_speech_silence_duration': 0.4,            # probably not needed
-        'min_length_of_recording': 0,                   # probably not needed? maybe?
+        'post_speech_silence_duration': 0.4,
+        'min_length_of_recording': 0,
         'min_gap_between_recordings': 0,
-        'enable_realtime_transcription': True,          # probably not needed? Start simple with just non-realtime transcription
-        'realtime_processing_pause': 0.2,                   # probably not needed
-        'realtime_model_type': 'tiny',                      # probably not needed
-        'on_realtime_transcription_update': text_detected,  # probably not needed
-        'silero_deactivity_detection': True,                # probably not needed
+        'enable_realtime_transcription': True,
+        'realtime_processing_pause': 0.2,
+        'realtime_model_type': 'tiny',
+        'on_realtime_transcription_update': text_detected,
+        'silero_deactivity_detection': True,
+        'use_microphone': False,
 }
 recorder = AudioToTextRecorder(**recorder_config)
-"""
 
 @client.event
 async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+#Define a slash command
+"""
+@client.tree.command()
+async def testge(interaction: discord.Interaction):
+    await interaction.response.send_message(f'hi, {interaction.user.mention}')
+"""
+
+@client.tree.command()
+async def dumpstate(interaction: discord.Interaction):
+    global transcription_tasks
+    print(f'Dumping transcription_tasks: {transcription_tasks}')
+    await interaction.response.send_message('ok. check log', ephemeral=True)
+
+async def JoinVoiceChannel(channel):
+    print(f'Joining voice channel: {channel}')
     pass
+    
+async def DisconnectVoiceChannel(channel):
+    print(f'Disconnecting from channel: {channel}')
+    pass
+
+"""
+Define `/transcribeme` command
+"""
+@client.tree.command()
+async def transcribeme(interaction: discord.Interaction):
+    #check if interaction.user is a host, housekeeping, or someone marked with the "transcription allowed" role
+    if len(set([role.id for role in interaction.user.roles]) & set(transcription_ok_roles.values())) == 0:
+        await interaction.response.send_message(f'You are not authorized to use this command.', ephemeral=True)
+        return
+    
+    #check if interaction.user is in a voice channel
+    if interaction.user.voice == None:
+        await interaction.response.send_message(f'You are not in a voice chat channel.  Connect to a voice chat channel and try this command again.', ephemeral=True)
+        return
+
+    global transcription_tasks
+    global active_voiceclient
+    
+    if interaction.user in transcription_tasks:
+        #stop listening to user
+        await active_voiceclient.disconnect()
+        active_voiceclient = None
+        del transcription_tasks[interaction.user]
+        await interaction.response.send_message(f'No longer transcribing for {interaction.user.mention}.')
+    else:
+        #limit to 1 user at a time
+        if len(transcription_tasks) > 0:
+            current_transcription_user = list(transcription_tasks.keys())[0]
+            await interaction.response.send_message(f'[WIP] Unable to start transcription service.  Transcription in use by {current_transcription_user.mention}')
+        else:
+            print(f'starting transcription, user dump: {interaction.user}')
+            print(f'VoiceState for user: {interaction.user.voice}')
+            transcription_tasks[interaction.user] = interaction.channel_id
+            active_voiceclient = await interaction.user.voice.channel.connect(
+                cls=voice_recv.VoiceRecvClient, # hook discord-ext-voice-recv here
+                self_mute=True
+            )
+            #discord-ext-voice-recv -specific stuff
+            def callback(user, data: voice_recv.VoiceData):
+                print(
+                    f'I heard something from {user}.\n'
+                    f'\tPacket: {data.packet}.\n'
+                    f'\tpcm length: {len(data.pcm)}.\n'
+                    f'\tsource: {data.source}.\n'
+                )
+                # do something with data, i.e. hand it off to RealtimeSTT somehow
+
+            active_voiceclient.listen(voice_recv.BasicSink(callback))
+
+            await interaction.response.send_message(f'[WIP] {interaction.user.mention} is now transcribing voice chat to this channel.')
+
+"""
+define something that happens when the person who started /transcribeme disconnects from voice chat
+"""
+@client.event
+async def on_voice_state_update(member, before, after):
+    if len(transcription_tasks) == 0:
+        #print(f'Not transcribing, doing nothing.')
+        return
+
+    #print(f'on_voice_state_update called with args:')
+    #print(f'Member: {member} ID: {member.id}')
+    #print(f'{before}')
+    #print(f'{after}')
+    global active_voiceclient
+    if member in transcription_tasks and before.channel is not None and after.channel is None:
+        await active_voiceclient.disconnect()
+        transcription_channel = client.get_channel(transcription_tasks[member])
+        del transcription_tasks[member]
+        await transcription_channel.send(f'No longer transcribing for {member.mention} (Disconnected from voice channel)')
+        
+"""
+define event handler for when voice chat
+NOTE: discord.py does not seem to provide actually receiving voice data
+"""
+
+
 
 with open('blipblap', 'r') as file:
     data = file.read()
-    client.run(data)
+    client.run(data) #blocks
+    
+#cleanup
+#recorder.stop()
